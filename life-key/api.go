091425690f7872ae220d6e76d93e886c2c933d5b
Garallz <garalinluzhi@gmail.com>
2017-06-07 11:11:22 +0800

    rm .idea

--- life-key/api.go
+++ life-key/api.go
@@ -18,11 +18,11 @@ type LifeData struct {
 
 func (d *LifeData)Set(key string,  life int64) {
 	d.mc.Lock()
-	defer d.mc.Unlock()
 	d.data[key] = &LifeKey{
 		Start:time.Now().Unix(),
 		Life:life,
 	}
+	d.mc.Unlock()
 }
 
 func (d *LifeData)Check(key string) bool {
@@ -47,28 +47,28 @@ func (d *LifeData)Get(key string) interface{} {
 
 func (d *LifeData)SetAddData(data interface{}, key string,  life int64) {
 	d.mc.Lock()
-	defer d.mc.Unlock()
 	d.data[key] = &LifeKey{
 		Data:data,
 		Start:time.Now().Unix(),
 		Life:life,
 	}
+	d.mc.Unlock()
 }
 
 func (d *LifeData)Delete(key string) {
 	d.mc.Lock()
-	defer d.mc.Unlock()
 	delete(d.data, key)
+	d.mc.Unlock()
 }
 
 // If data not update, input nil.
 func (d *LifeData)UpdateData(data interface{}, key string) {
 	d.mc.Lock()
-	defer d.mc.Unlock()
 	d.data[key].Start = time.Now().Unix()
 	if data != nil {
 		d.data[key].Data = data
 	}
+	d.mc.Unlock()
 }
 
 func (d *LifeData)GcData(life time.Duration) {
