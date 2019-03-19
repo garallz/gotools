876b95039af1c3634857f1ff3cb217122d461373
Gara <garalinluzhi@gmail.com>
2019-03-19 10:29:27 +0000

    update to version 0.1

--- router/README.md
+++ router/README.md
@@ -1,15 +1,16 @@
-# Golang Router
+# Golang Router (version: 0.1)
 
 ## Base on Golang net/http
 
-### Auto deal with response body to json and xml
+### deal with response body to json and xml
 
 ### Make request uid
-    - uid = IP + Time + Rand
+    - uid = IP + Time + Rand (28 bytes)
 
 ### Make request log and response log
 
 ```go
+	// see router_test.go
 
 	s := NewRouter(&http.Server{
 		Addr: ":9080",
@@ -17,3 +18,18 @@
 	s.Post("/sandbox", Sandbox)
 	s.Run()
 ```
+
+## Downloa and update use package
+```sh
+	git clone github.com/garallz/Go/tree/master/router
+```
+- You can download the package on your program and update to do
+- Like change the log write or request (response) deal with
+- When you do it, should be declared to use
+
+
+## Need to do with next version
+
+- log write system
+- safety to close server
+- find router path by tree
--- router/router.go
+++ router/router.go
@@ -1,92 +1,111 @@
 package router
 
 import (
+	"errors"
 	"net/http"
 	"strings"
 )
 
+// Set glabol server
+var glabol *Server
+
+type Server struct {
+	maxRead     int64
+	content     ContentType
+	server      *http.Server
+	contentType ContentType
+	routers     map[string]*RouterPath
+}
+
 type RouterPath struct {
 	method   string
 	path     string
-	function func(*CommRouter)
+	function []func(*CommRouter)
 }
 
-type Server struct {
-	content ContentType
-	server  *http.Server
-}
-
-var defaultContentType = ContentTypeNon
-var routers map[string]*RouterPath
-
-func GetFunction(path string) *RouterPath {
-	return routers[path]
-}
-
-func SetDefaultContentType(contentType string) {
-	switch strings.ToUpper(contentType) {
-	case "json":
-		defaultContentType = ContentTypeJson
-	case "xml":
-		defaultContentType = ContentTypeXml
-	case "byte":
-		defaultContentType = ContentTypeByte
+// make new router
+func NewRouter(server *http.Server) *Server {
+	return &Server{
+		maxRead:     1024 * 1024,
+		server:      server,
+		contentType: ContentTypeNon,
+		routers:     make(map[string]*RouterPath),
 	}
 }
 
-func NewRouter(server *http.Server) *Server {
-	routers = make(map[string]*RouterPath)
-	return &Server{server: server}
+// when use response convert, cat default content type
+// json, xml, byte
+func (s *Server) SetDefaultContentType(contentType string) {
+	switch strings.ToLower(contentType) {
+	case "json":
+		s.contentType = ContentTypeJson
+	case "xml":
+		s.contentType = ContentTypeXml
+	case "byte":
+		s.contentType = ContentTypeByte
+	default:
+		panic("Set Default Content-Type Wrong")
+	}
 }
 
-func (s *Server) Post(path string, function func(*CommRouter)) {
-	routers[path] = &RouterPath{
+// max requesty body read, default: 1024*1024 bytes
+func (s *Server) SetMaxBodyRead(number int) error {
+	if number < 512 {
+		return errors.New("Min body lenght is 512 bytes")
+	}
+	s.maxRead = int64(number)
+	return nil
+}
+
+func (s *Server) Post(path string, functions ...func(*CommRouter)) {
+	s.routers[path] = &RouterPath{
 		method:   "POST",
 		path:     path,
-		function: function,
+		function: functions,
 	}
 }
 
-func (s *Server) Get(path string, function func(*CommRouter)) {
-	routers[path] = &RouterPath{
+func (s *Server) Get(path string, functions ...func(*CommRouter)) {
+	s.routers[path] = &RouterPath{
 		method:   "GET",
 		path:     path,
-		function: function,
+		function: functions,
 	}
 }
 
-func (s *Server) Put(path string, function func(*CommRouter)) {
-	routers[path] = &RouterPath{
+func (s *Server) Put(path string, functions ...func(*CommRouter)) {
+	s.routers[path] = &RouterPath{
 		method:   "PUT",
 		path:     path,
-		function: function,
+		function: functions,
 	}
 }
 
-func (s *Server) Delete(path string, function func(*CommRouter)) {
-	routers[path] = &RouterPath{
+func (s *Server) Delete(path string, functions ...func(*CommRouter)) {
+	s.routers[path] = &RouterPath{
 		method:   "DELETE",
 		path:     path,
-		function: function,
+		function: functions,
 	}
-}
-
-var reqCommDeal struct {
-	check    bool
-	function func(*CommRouter)
-}
-
-// Deal with common function with request
-// defore body function
-func (s *Server) CommonReqFunction(function func(*CommRouter)) {
-	reqCommDeal.check = true
-	reqCommDeal.function = function
 }
 
 func (s *Server) Run() {
-	s.server.Handler = http.HandlerFunc(CommonDealRequest)
+	glabol = s
+	glabol.server.Handler = http.HandlerFunc(CommonDealWith)
 
-	if err := s.server.ListenAndServe(); err != nil {
+	if err := glabol.server.ListenAndServe(); err != nil {
 		panic(err)
 	}
+}
+
+func getContentType() ContentType {
+	return glabol.contentType
+}
+
+func getFunction(path string) *RouterPath {
+	return glabol.routers[path]
+}
+
+func getMaxBodyRead() int64 {
+	return glabol.maxRead
 }
